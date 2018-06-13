09df2e945b936d41acfbeeee594ddc6f9bdd5959
zzgbsh <zzgbsh@126.com>
2018-06-13 09:31:18 +0800

    add filePath

--- path.js
+++ path.js
@@ -2,3 +2,13 @@
 
 var url = require('url');
 console.log(url.parse('http://user:pass@host.com:8080/path/to/file?query=string#hash'));
+
+var path = require('path');
+
+// 解析当前目录
+var workDir = path.resolve('.');
+
+//组合完整的路径
+var filePath = path.join(workDir,'pub','index.html');//E:\gits\node-excercise\pub\index.html
+
+console.log(filePath);
